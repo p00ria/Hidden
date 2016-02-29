a8776ebc92f701deaab2d6265db6a6a8f16f69d4
p00ria <amir1300sobatdar@gmail.com>
2016-02-29 17:38:35 +0330

    Update Xy.lua

--- plugins/Xy.lua
+++ plugins/Xy.lua
@@ -1,12 +1,12 @@
 do
 local function run(msg, matches)
-local bot_id = our_id 
-local fbot1 = 180087690 --ایدی اینویت کننده رو اینجا بنویسید
+local bot_id = -- آیدی اینوایت کننده رو اینجا بنویسید --
+local fbot1 = -- آیدی اینوایت شونده را اینجا بنویسید --
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
     end
 end
- --channel : @nod32team
+
 return {
   patterns = {
     "^!!tgservice (.+)$",
@@ -14,6 +14,4 @@ return {
   run = run
 }
 end
---open xy git by : @behroozyaghi
---channel : @nod32team
---کپی بدون ذکر منبع حرام است
+
